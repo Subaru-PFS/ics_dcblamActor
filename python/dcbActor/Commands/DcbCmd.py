149c1e6f968a71cacaa14ee8c79c12ecaeebe6cd
Arnaud Le Fur <alefur@lam.fr>
2016-11-28 20:15:04 +0000

    first version working, status ok

--- python/dcbActor/Commands/DcbCmd.py
+++ python/dcbActor/Commands/DcbCmd.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import sys
+
 import opscore.protocols.keys as keys
 import opscore.protocols.types as types
 
@@ -30,15 +32,39 @@ class DcbCmd(object):
         """Query the actor for liveness/happiness."""
         cmd.finish("text='Present and (probably) well'")
 
-    def status(self, cmd):
+    def status(self, cmd, channel=None):
         """Report status and version; obtain and send current data"""
+        # if not self.actor.initOk:
+        #     cmd.finish("text='Present and (probably) well'")
+        #     self.actor.initOk = 1
+        #     return
 
-        cmd.inform('text="Present!"')
-        cmd.finish()
+        config = self.actor.config
+        options = config.options("address")
+        if channel is None:
+            for option in options:
+                try:
+                    channelNb = config.get("address", option)
+                    status = self.actor.getStatus(cmd, channelNb)
+                    cmd.inform("%s=%s" % (option, status))
+                except Exception as e:
+                    cmd.warn("text='getStatus %s has failed %s'" %
+                             (option, self.actor.formatException(e, sys.exc_info()[2])))
+
+            cmd.finish()
+        else:
+            cmd.finish(cmd.inform("%s=%s" % (channel, self.actor.getStatus(cmd, config.get("address", channel)))))
+
 
     def switch(self, cmd):
         cmdKeys = cmd.cmd.keywords
         channel = cmdKeys["channel"].values[0]
-        
-        ret = self.actor.compute(expTime)
-        cmd.finish("result=%.2f" % ret)
+        bool = "on" if "on" in cmdKeys else "off"
+
+        try:
+            ret = self.actor.switch(cmd, channel, bool)
+        except Exception as e:
+            cmd.warn("text='switch %s has failed %s'" % (channel, self.actor.formatException(e, sys.exc_info()[2])))
+            self.actor.sock = None
+
+        self.status(cmd, channel)
--- python/dcbActor/main.py
+++ python/dcbActor/main.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+import socket
+import time
+from datetime import datetime as dt
+
 from actorcore.Actor import Actor
 
 
@@ -7,13 +11,145 @@ class DcbActor(Actor):
     def __init__(self, name, productName=None, configFile=None, debugLevel=30):
         # This sets up the connections to/from the hub, the logger, and the twisted reactor.
         #
+        self.initOk = 0
         Actor.__init__(self, name,
                        productName=productName,
                        configFile=configFile)
+        self.sock = None
+        self.tcpHost = self.config.get(name, 'tcp_host')
+        self.tcpPort = int(self.config.get(name, 'tcp_port'))
+        self.EOL = '\r\n'
+
+    def switch(self, cmd, channel, bool):
+
+        address = self.config.get('address', channel)
+        return self.sendOneCommand("sw o%s %s" % (address.zfill(2), bool), doClose=False, cmd=cmd)
+
+    def getStatus(self, cmd, channel):
+
+        ret = self.sendOneCommand("read status o%s format" % channel.zfill(2), doClose=False, cmd=cmd)
+        return "on" if ' on' in ret else "off"
+
+    def formatException(self, e, traceback=""):
+
+        return "%s %s %s" % (str(type(e)).replace("'", ""), str(type(e)(*e.args)).replace("'", ""), traceback)
+
+    def connectSock(self, i=0):
+        """ Connect socket if self.sock is None
+
+        :param cmd : current command,
+        :return: sock in operation
+                 bsh simulator in simulation
+        """
+
+        if self.sock is None:
+            try:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.settimeout(5.0)
+            except Exception as e:
+                raise Exception("%s failed to create socket : %s" % (self.name, self.formatException(e)))
+
+            try:
+                s.connect((self.tcpHost, self.tcpPort))
+                time.sleep(0.1)
+                if s.recv(1024)[-7:] != "Login: ":
+                    if i > 2:
+                        raise Exception("weird")
+                    else:
+                        return self.connectSock(i + 1)
+                s.send("teladmin%s" % self.EOL)
+                time.sleep(0.1)
+                if s.recv(1024)[-10:] != "Password: ":
+                    if i > 2:
+                        raise Exception("bad login")
+                    else:
+                        return self.connectSock(i + 1)
+                s.send("pfsait%s" % self.EOL)
+                time.sleep(0.1)
+                if "Logged in successfully" not in s.recv(1024):
+                    if i > 2:
+                        raise Exception("bad password")
+                    else:
+                        return self.connectSock(i + 1)
+
+            except Exception as e:
+                raise Exception("%s failed to connect socket : %s" % (self.name, self.formatException(e)))
+
+            self.sock = s
+
+        return self.sock
+
+    def closeSock(self):
+        """ close socket
+
+        :param cmd : current command,
+        :return: sock in operation
+                 bsh simulator in simulation
+        """
+
+        if self.sock is not None:
+            try:
+                self.sock.close()
+
+            except Exception as e:
+                raise Exception("%s failed to close socket : %s" % (self.name, self.formatException(e)))
+
+        self.sock = None
+
+    def sendOneCommand(self, cmdStr, doClose=True, cmd=None):
+        """ Send one command and return one response.
+
+        Args
+        ----
+        cmdStr : str
+           The command to send.
+        doClose : bool
+           If True (the default), the device socket is closed before returning.
+
+        Returns
+        -------
+        str : the single response string, with EOLs stripped.
+
+        Raises
+        ------
+        IOError : from any communication errors.
+        """
+
+        if cmd is None:
+            cmd = self.bcast
+
+        fullCmd = "%s%s" % (cmdStr, self.EOL)
+        self.logger.debug('sending %r', fullCmd)
+
+        s = self.connectSock()
+
+        try:
+            s.sendall(fullCmd)
+
+        except Exception as e:
+            raise Exception("%s failed to send %s : %s" % (self.name.upper(), fullCmd, self.formatException(e)))
+
+        reply = self.getOneResponse(sock=s, cmd=cmd)
+        if doClose:
+            self.closeSock()
+
+        return reply
+
+    def getOneResponse(self, sock=None, cmd=None):
+        if sock is None:
+            sock = self.connectSock()
+        t0 = dt.now()
+        reply = sock.recv(1024)
+
+        while '\r\n>' not in reply:
+            if (dt.now() - t0).total_seconds() > 10:
+                raise Exception("timeout")
+            reply += sock.recv(1024)
+
+        #return reply.split('\r\n> \xff\xf1')[0].split(self.EOL)[1]
+        return reply.split('\r\n>')[0].split(self.EOL)[1]
 
 
-    def compute(self, expTime):
-        return 10*expTime
 
 def main():
     actor = DcbActor('dcb', productName='dcbActor')
