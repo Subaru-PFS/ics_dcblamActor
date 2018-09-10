9c942e95164d68c394d734c5a5d14265cde81d36
Arnaud Le Fur <arnaud.lefur@lam.fr>
2018-09-10 19:09:53 +0200

    minimal simulator

--- python/dcbActor/Controllers/simulator/monosim.py
+++ python/dcbActor/Controllers/simulator/monosim.py
@@ -10,6 +10,11 @@ class Monosim(socket.socket):
         self.recv = self.fakeRecv
 
         self.buf = []
+        self.handshake = 0
+        self.shutterOpen = False
+        self.grating = 1
+        self.outport = 1
+        self.wavelength = 300
 
     def connect(self, server):
         (ip, port) = server
@@ -22,11 +27,52 @@ class Monosim(socket.socket):
     def fakeSend(self, cmdStr):
         time.sleep(0.1)
         cmdStr = cmdStr.decode()
+        cmdStr = cmdStr.split('\r\n')[0]
+        funcname = cmdStr.split(',')[0]
+        args = cmdStr.split(',')[1:]
 
-        if cmdStr == 'status\r\n':
+        if funcname == 'status':
             self.buf.append('0,Oriel monochromator\r\n')
+
+        elif funcname == 'init':
+            self.handshake = 1
+            self.buf.append('0,%d\r\n' % self.handshake)
+
+        elif funcname == 'getshutter':
+            shutter = 'O' if self.shutterOpen else 'C'
+            self.buf.append('0,%s\r\n' % shutter)
+
+        elif funcname == 'getgrating':
+            self.buf.append('0,%d,1200,BLUE\r\n' % self.grating)
+
+        elif funcname == 'getoutport':
+            self.buf.append('0,%d\r\n' % self.outport)
+
+        elif funcname == 'getwave':
+            self.buf.append('0,%.3f\r\n' % self.wavelength)
+
+        elif funcname == 'shutteropen':
+            self.shutterOpen = True
+            self.buf.append('0,O\r\n')
+
+        elif funcname == 'shutterclose':
+            self.shutterOpen = False
+            self.buf.append('0,C\r\n')
+
+        elif funcname == 'setgrating':
+            self.grating = int(args[0])
+            self.buf.append('0,%d,1200,BLUE\r\n' % self.grating)
+
+        elif funcname == 'setoutport':
+            self.outport = int(args[0])
+            self.buf.append('0,%d\r\n' % self.outport)
+
+        elif funcname == 'setwave':
+            self.wavelength = float(args[0])
+            self.buf.append('0,%.3f\r\n' % self.wavelength)
+
         else:
-            self.buf.append('1,unknown command\r\n')
+            self.buf.append('1,unknown command %s\r\n'%cmdStr)
 
     def fakeRecv(self, buffer_size):
         ret = self.buf[0]
