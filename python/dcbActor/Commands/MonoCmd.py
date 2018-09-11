c489af6c34890452fc87d0797d200f38ff0fbd8b
Arnaud Le Fur <arnaud.lefur@lam.fr>
2018-09-11 12:37:46 +0200

    basic monochromator commands

--- python/dcbActor/Commands/MonoCmd.py
+++ python/dcbActor/Commands/MonoCmd.py
@@ -19,10 +19,19 @@ class MonoCmd(object):
         self.name = "mono"
         self.vocab = [
             (self.name, 'status', self.status),
+            (self.name, 'init', self.initialise),
+            (self.name, '@(shutter) @(open|close)', self.cmdShutter),
+            (self.name, '@(set) <grating>', self.setGrating),
+            (self.name, '@(set) <outport>', self.setOutport),
+            (self.name, '@(set) <wave>', self.setWave),
+
         ]
 
         # Define typed command arguments for the above commands.
         self.keys = keys.KeysDictionary("dcb__mono", (1, 1),
+                                        keys.Key("grating", types.Int(), help="Grating Id"),
+                                        keys.Key("outport", types.Int(), help="Outport Id"),
+                                        keys.Key("wave", types.Float(), help="Wavelength"),
                                         )
 
     @property
@@ -38,3 +47,49 @@ class MonoCmd(object):
 
         self.controller.getStatus(cmd)
 
+    @threaded
+    def initialise(self, cmd):
+        """Initialise Bsh, call fsm startInit event """
+
+        self.controller.substates.init(cmd=cmd)
+        self.controller.getStatus(cmd)
+
+    @threaded
+    def cmdShutter(self, cmd):
+        """Open/close , optional keyword force to force transition (without breaking interlock)"""
+        cmdKeys = cmd.cmd.keywords
+
+        openShutter = True if "open" in cmdKeys else "close"
+
+        self.controller.setShutter(cmd=cmd, openShutter=openShutter)
+        self.controller.getStatus(cmd)
+
+    @threaded
+    def setGrating(self, cmd):
+        """Update bia parameters """
+
+        cmdKeys = cmd.cmd.keywords
+        gratingId = int(cmdKeys["grating"].values[0])
+
+        self.controller.setGrating(cmd=cmd, gratingId=gratingId)
+        self.controller.getStatus(cmd)
+
+    @threaded
+    def setOutport(self, cmd):
+        """Update bia parameters """
+
+        cmdKeys = cmd.cmd.keywords
+        outportId = int(cmdKeys["outport"].values[0])
+
+        self.controller.setOutport(cmd=cmd, outportId=outportId)
+        self.controller.getStatus(cmd)
+
+    @threaded
+    def setWave(self, cmd):
+        """Update bia parameters """
+
+        cmdKeys = cmd.cmd.keywords
+        wavelength = float(cmdKeys["wave"].values[0])
+
+        self.controller.setWave(cmd=cmd, wavelength=wavelength)
+        self.controller.getStatus(cmd)
