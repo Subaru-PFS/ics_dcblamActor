15a0b4aa4a6454afead676b9b597ee599f885ffb
Arnaud Le Fur <alefur@lam.fr>
2019-02-15 14:32:15 +0000

    typo

--- python/dcbActor/main.py
+++ python/dcbActor/main.py
@@ -97,8 +97,9 @@ class OurActor(actorcore.ICC.ICC):
         conf = configparser.ConfigParser()
         conf.read_file(open('/software/ait/fiberConfig.cfg'))
         fibers = [fib.strip() for fib in conf.get('current', 'fibers').split(',')]
-        cmd.inform('fibers=%s' % ','.join(fibers))
         pfiDesignId = lamConfig.hashColors(fibers)
+
+        cmd.inform('fiberConfig=%s' % ','.join(fibers))
         cmd.inform('designId=0x%016x' % pfiDesignId)
 
 
